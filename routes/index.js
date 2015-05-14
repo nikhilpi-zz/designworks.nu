19f50456b93042eaee8907ada174f27a02fcd791
nikhilpi <nikhilpai2016@u.northwestern.edu>
2015-05-13 19:30:24 -0500

    invoice gen

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var hummus = require('hummus');
+var _ = require('underscore');
+var moment = require('moment');
 var fs = require('fs');
 var router = express.Router();
 var nodemailer = require('nodemailer');
@@ -56,18 +58,111 @@ router.post('/mail', function(request, response) {
   });
 });
 
+router.get('/invoiceForm',function(req, res) {
+  res.render('invoice');
+});
 
-router.get('/invoice', function(req, res) {
+router.post('/invoice', function(req, res) {
+  console.log(req);
+  var params = req.body;
+  // var params = {
+  //   to: {
+  //     org: 'NSH',
+  //     address: '604 Davis St, Unit 4',
+  //     email: 'nikhil@gmail.com',
+  //     clientNum: '111',
+  //     date: '11/11/2016'
+  //   },
+  //   items: [
+  //     {
+  //       service: 'Flyer',
+  //       pricePer: '$500',
+  //       amount: '3',
+  //       total: '$1500'
+  //     },
+  //     {
+  //       service: 'Flyer',
+  //       pricePer: '$500',
+  //       amount: '3',
+  //       total: '$1500'
+  //     },
+  //     {
+  //       service: 'Flyer',
+  //       pricePer: '$500',
+  //       amount: '3',
+  //       total: '$1500'
+  //     },
+  //     {
+  //       service: 'Flyer',
+  //       pricePer: '$500',
+  //       amount: '3',
+  //       total: '$1500'
+  //     }
+  //   ],
+  //   total: '$5000'
+  // };
   
   var pdfWriter = hummus.createWriterToModify('./pdf/invoice_template.pdf',{modifiedFilePath:'./pdf/new_invoice_template.pdf'});
-  testPageSizeModification(pdfWriter)
-  // var pageModifier = new hummus.PDFPageModifier(pdfWriter,0);
+  var pageModifier = new hummus.PDFPageModifier(pdfWriter,0);
 
-  // pageModifier.startContext().getContext().writeText('Test Text',
-  //                         0,
-  //                         0,
-  //                         {font:pdfWriter.getFontForFile('./TestMaterials/fonts/Couri.ttf'),size:14,colorspace:'gray',color:0xFF});
-  // pageModifier.endContext().writePage();
+  //TO
+  pageModifier.startContext().getContext().writeText(params.to.org,
+                          80,
+                          560,
+                          {font:pdfWriter.getFontForFile('./fonts/Futura.ttc'),size:14,colorspace:'gray',color:0xFF});
+  pageModifier.startContext().getContext().writeText(params.to.address,
+                          80,
+                          543,
+                          {font:pdfWriter.getFontForFile('./fonts/Futura.ttc'),size:14,colorspace:'gray',color:0xFF});
+  pageModifier.startContext().getContext().writeText(params.to.email,
+                          80,
+                          527,
+                          {font:pdfWriter.getFontForFile('./fonts/Futura.ttc'),size:14,colorspace:'gray',color:0xFF});
+  pageModifier.startContext().getContext().writeText(params.to.clientNum,
+                          87,
+                          499,
+                          {font:pdfWriter.getFontForFile('./fonts/Futura.ttc'),size:12,colorspace:'gray',color:0xFF});
+  pageModifier.startContext().getContext().writeText(params.to.clientNum,
+                          197,
+                          499,
+                          {font:pdfWriter.getFontForFile('./fonts/Futura.ttc'),size:12,colorspace:'gray',color:0xFF});
+  pageModifier.startContext().getContext().writeText(moment(params.to.date).format("MM/DD/YYYY"),
+                          250,
+                          499,
+                          {font:pdfWriter.getFontForFile('./fonts/Futura.ttc'),size:12,colorspace:'gray',color:0xFF});
+
+  //items
+  var itemY = 362;
+  _.each(params.items, function(item){
+    if(item.service){
+      pageModifier.startContext().getContext().writeText(item.service,
+                            55,
+                            itemY,
+                            {font:pdfWriter.getFontForFile('./fonts/steelfish_rg.otf'),size:28,colorspace:'gray',color:0xFF});
+      pageModifier.startContext().getContext().writeText("$" + item.pricePer.toString(),
+                            327,
+                            itemY,
+                            {font:pdfWriter.getFontForFile('./fonts/steelfish_rg.otf'),size:28,colorspace:'gray',color:0xFF});
+      pageModifier.startContext().getContext().writeText(item.amount,
+                            420,
+                            itemY,
+                            {font:pdfWriter.getFontForFile('./fonts/steelfish_rg.otf'),size:28,colorspace:'gray',color:0xFF});
+      pageModifier.startContext().getContext().writeText("$" + item.total.toString(),
+                            495,
+                            itemY,
+                            {font:pdfWriter.getFontForFile('./fonts/steelfish_rg.otf'),size:28,colorspace:'gray',color:0xFF});
+    }
+    itemY -= 85;
+  });
+
+  //Total
+  pageModifier.startContext().getContext().writeText("$" + params.total.toString(),
+                          415,
+                          37,
+                          {font:pdfWriter.getFontForFile('./fonts/Futura.ttc'),size:25,colorspace:'gray',color:0xFF});
+  
+
+  pageModifier.endContext().writePage();
 
   pdfWriter.end();
 
