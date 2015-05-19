61021f5bd94b15cde0d2f1541155c36ed1befa09
nikhilpi <nikhilpai2016@u.northwestern.edu>
2015-05-18 22:51:09 -0500

    recaptcha

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var hummus = require('hummus');
 var _ = require('underscore');
 var moment = require('moment');
-var fs = require('fs');
+var request = require('request');
+var async = require('async');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
@@ -39,22 +40,38 @@ router.get('/apply', function(req, res) {
   res.render('apply');
 });
 
-router.post('/mail', function(request, response) {
+router.post('/mail', function(req, res) {
+  var cap = req.body['g-recaptcha-response'];
 
-  transporter.sendMail({
-    from: 'designworks.nu@gmail.com',
-    to: 'designworks.nu@gmail.com',
-    subject: 'Service request ' + request.body.Service + " from " + request.body.Name,
-    text: 'Service request from ' +  request.body.Name + '\n' + request.body.Email + '\n' + request.body.Message
-  }, function(){
-    transporter.sendMail({
-      from: 'designworks.nu@gmail.com',
-      to: request.body.Email,
-      subject: 'Thank you for your ' + request.body.Service + ' request',
-      text: 'Hello ' +  request.body.Name + '!\n\nWe have received your design request, and a member from the DesignWorks team will contact you within 24 Hours regarding the details of your request. If this request is urgent, please contact us at (847)-868-0418, or email us at\nhello@designworks.nu\n\nHave a nice day!\nThe DesignWorks Team'
-    }, function(res){
-      response.redirect('/process');
-    });
+  request.post({
+    url:'https://www.google.com/recaptcha/api/siteverify', 
+    form: { secret: '6LfpEQcTAAAAAHOer9Gk0lnDvW3MpFftJ6FGnPg5', response: cap }}, 
+    function(err,httpResponse,body){ 
+      var data = JSON.parse(body)
+      if(data['success']){
+        async.parallel([
+            function(callback){
+                transporter.sendMail({
+                  from: 'designworks.nu@gmail.com',
+                  to: 'designworks.nu@gmail.com',
+                  subject: 'Service request ' + req.body.Service + " from " + req.body.Name,
+                  text: 'Service request from ' +  req.body.Name + '\n' + req.body.Email + '\n' + req.body.Message
+                }, callback);
+            },
+            function(callback){
+              transporter.sendMail({
+                from: 'designworks.nu@gmail.com',
+                to: req.body.Email,
+                subject: 'Thank you for your ' + req.body.Service + ' request',
+                text: 'Hello ' +  req.body.Name + '!\n\nWe have received your design request, and a member from the DesignWorks team will contact you within 24 Hours regarding the details of your req. If this request is urgent, please contact us at (847)-868-0418, or email us at\nhello@designworks.nu\n\nHave a nice day!\nThe DesignWorks Team'
+              }, callback);
+            }
+        ],
+        // optional callback 
+        function(err, results){
+            res.redirect('/process');
+        });
+      }
   });
 });
 
